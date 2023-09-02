340336e29538ea7161c3a9ae0c8542852ca69c5e
unknown <xluixarturox123@gmail.com>
2023-09-01 22:51:04 -0500

    Agregando la interfaz de productos

--- index.php
+++ index.php
@@ -3,71 +3,90 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <title>Document</title>
+    <script src="https://checkout.culqi.com/js/v4"></script>
 </head>
 <body>
-<script src="https://checkout.culqi.com/js/v4"></script>
-  <script>
-    Culqi.publicKey = 'pk_test_XBWsfPU0w7KmcLF9';
+    <div class="container text-center">
+        <h1>Productos</h1>
+        <div class="row">
+            <div class="col-4">
+                <div class="card">
+                    <img src="img/polo.jpg" class="card-img-top" alt="Polo Adidas">
+                    <div class="card-body">
+                        <h5 class="card-title">Polo Adidas</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a class="btn btn-primary" id="btn_pagar">Comprar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <img src="img/polo2.jpg" class="card-img-top" alt="Polo Adidas">
+                    <div class="card-body">
+                        <h5 class="card-title">Polo Adidas</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a class="btn btn-primary" id="btn_pagar">Comprar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <img src="img/polo3.jpg" class="card-img-top" alt="Polo Adidas">
+                    <div class="card-body">
+                        <h5 class="card-title">Polo Adidas</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a class="btn btn-primary" id="btn_pagar">Comprar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-
-
-
-
-
-  /*Culqi.options({
-      style: {
-        logo: 'https://culqi.com/LogoCulqi.png',
-        bannerColor: '', // hexadecimal
-        buttonBackground: '', // hexadecimal
-        menuColor: '', // hexadecimal
-        linksColor: '', // hexadecimal
-        buttonText: '', // texto que tomará el botón
-        buttonTextColor: '', // hexadecimal
-        priceColor: '' // hexadecimal
-      }
-  });
-  */
-
-  const btn_pagar = document.getElementById('btn_pagar');
-
-btn_pagar.addEventListener('click', function (e) {
-    // Abre el formulario con la configuración en Culqi.settings y CulqiOptions
-
-        //SETTINGS CULQI
+    <script>
+        Culqi.publicKey = 'pk_test_XBWsfPU0w7KmcLF9';
+      
         Culqi.settings({
-        title: 'Culqi Store',
-        currency: 'PEN',  // Este parámetro es requerido para realizar pagos yape
-        amount: 1000,  // Este parámetro es requerido para realizar pagos yape
-        //order: 'ord_live_0CjjdWhFpEAZlxlz', // Este parámetro es requerido para realizar pagos con pagoEfectivo, billeteras y Cuotéalo
-
-
-
-
-    //OPCIONES DE PAGO
+            title: 'Culqi Store',
+            currency: 'PEN',
+            amount: 1000,
+            order: 'ord_live_0CjjdWhFpEAZlxlz'
+        });
+      
         Culqi.options({
-        lang: "auto",
-        installments: false, // Habilitar o deshabilitar el campo de cuotas
-        paymentMethods: {
-        tarjeta: true,
-        yape: true,
-        bancaMovil: true,
-        agente: true,
-        billetera: true,
-        cuotealo: false,
-        },
-    });
-    });
-    Culqi.open();
-    e.preventDefault();
-})
+            lang: "auto",
+            installments: false,
+            paymentMethods: {
+                tarjeta: true,
+                yape: true,
+                bancaMovil: true,
+                agente: true,
+                billetera: true,
+                cuotealo: false,
+            },
+        });
+      
+        const btn_pagar = document.getElementById('btn_pagar');
+      
+        btn_pagar.addEventListener('click', function (e) {
+            Culqi.open();
+            e.preventDefault();
+        });
+      
+        function culqi() {
+            if (Culqi.token) {  
+                const token = Culqi.token;
+                console.log(`Se ha creado el objeto Token: ${token}.`);
+            } else if (Culqi.order) {  
+                const order = Culqi.order;
+                console.log(`Se ha creado el objeto Order: ${order}. para PagoEfectivo`);
+            } else {
+                console.log(`Error : ${Culqi.error.merchant_message}.`);
+            }
+        };
+    </script>
 
-  </script>
-
-
- 
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
